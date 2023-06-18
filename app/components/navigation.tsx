ce3defeacfdff4b1511a4c0059c8a82762e3019a
syu.onoda <s-onoda@creative-innovation.co.jp>
2023-06-18 09:47:02 +0900

    supabase連携

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -1,22 +1,30 @@
 "use client";
 
 import Link from "next/link";
+import useStore from "../../store";
 
 // ナビゲーション
 const Navigation = () => {
+  const { user } = useStore();
+
   return (
     <header className="border-b py-5">
       <div className="container max-w-screen-xl mx-auto relative flex justify-center items-center">
         <Link href="/" className=" font-bold text-xl cursor-pointer">
-          SyuTechBlog
+          FullStackChannel
         </Link>
 
         <div className="absolute right-5">
-          <div className="flex space-x-4">
-            <Link href="/auth/profile">プロフィール</Link>
-            <Link href="/auth/login">ログイン</Link>
-            <Link href="/auth/signup">サインアップ</Link>
-          </div>
+          {user.id ? (
+            <div className="flex space-x-4">
+              <Link href="/auth/profile">プロフィール</Link>
+            </div>
+          ) : (
+            <div className="flex space-x-4">
+              <Link href="/auth/login">ログイン</Link>
+              <Link href="/auth/signup">サインアップ</Link>
+            </div>
+          )}
         </div>
       </div>
     </header>
